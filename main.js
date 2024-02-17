fb43431aea823cf2ca66761c40f71610642fd9a5
sulemankh223322 <159639439+sulemankh223322@users.noreply.github.com>
2024-02-17 22:40:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question1
+console.log("hello world");
+// Question2
+var Myname = 'suleman khan';
+var message = 'Is a good person';
+console.log(Myname + "" + message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question1
+console.log("hello world");
+
+
+// Question2
+let Myname = 'suleman khan ';
+let message = ' is a good person';
+console.log(Myname+""+message)
